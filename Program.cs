071b96c7b52410eecb101e89d559a4a5af316866
David Sirait <davidsirait1156@gmail.com>
2025-06-23 20:53:52 +0700

    update CS classes

--- Program.cs
+++ Program.cs
@@ -1,27 +1,9 @@
-﻿using System;
-using System.Runtime.InteropServices;
+using System;
 
-enum Symbol
-{
-    Degree,
-    Radian
-}
+namespace DYNAMIC_CS_CPP_HAV;
 
-/* Compile
- * dotnet publish -c Release -r win-x64 --self-contained true /p:PublishAot=true /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true /p:StripSymbols=true /p:IlcDisableReflection=true /p:IlcGenerateCompleteTypeMetadata=false /p:InvariantGlobalization=true /p:EnableCompressionInSingleFile=true
- */
-class Program
-{
-    [DllImport("Hello.dll", CallingConvention = CallingConvention.Cdecl)]
-    public static extern void Hello();
-    [DllImport("Hello.dll", CallingConvention = CallingConvention.Cdecl)]
-    public static extern void Hav(double x, Symbol symbol = Symbol.Degree, bool printing = false);
-
-    static void Main()
-    {
-        Console.WriteLine("Calling C++ from C#...");
-        Hello();
-        Hav(90, printing:true);
-        Console.WriteLine("Done.");
+class Program{
+    public static void Main() {
+        Console.WriteLine("Hello");
     }
 }
---
+++ Program_test.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+/* Compile: 
+ * dotnet publish -c Release -r win-x64 --self-contained true
+ * /p:PublishAot=true
+ * /p:PublishSingleFile=true
+ * /p:IncludeNativeLibrariesForSelfExtract=true
+ * /p:StripSymbols=true
+ * /p:IlcDisableReflection=true
+ * /p:IlcGenerateCompleteTypeMetadata=false
+ * /p:InvariantGlobalization=true
+ * /p:EnableCompressionInSingleFile=true
+ */
+// using System.Runtime.InteropServices;
+/*enum Symbol
+{
+    Degree,
+    Radian
+}*/
+/*class Program_test 
+{
+    [DllImport("Hello.dll", CallingConvention = CallingConvention.Cdecl)]
+    static extern void Hello();
+    [DllImport("Hello.dll", CallingConvention = CallingConvention.Cdecl)]
+    static extern void Hav(double x, Symbol symbol = Symbol.Degree, bool printing = false);
+
+    static void Main_test()
+    {
+        Console.WriteLine("Calling C++ from C#...");
+        Hello();
+        Hav(90, printing:true);
+        Console.WriteLine("Done.");
+    }
+}*/
+
