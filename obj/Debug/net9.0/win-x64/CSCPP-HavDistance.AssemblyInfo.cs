160ed46da2f2882992e070df644b4271f3342b19
Retro <davidsirait1156@gmail.com>
2025-07-22 01:18:49 +0700

    update dll (linux)

--- obj/Debug/net9.0/win-x64/CSCPP-HavDistance.AssemblyInfo.cs
+++ obj/Debug/net9.0/win-x64/CSCPP-HavDistance.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Haversine calculator with Hybrid-Programming C/C++ and C#")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8c53bff79e1f3554c85d2646351f096c98f9774")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdc2ca538fac016eb808e0d270e78b92dd32af89")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSCPP-HavDistance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSCPP-HavDistance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
